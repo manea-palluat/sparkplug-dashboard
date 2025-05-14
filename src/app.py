2ab182edddf5ba5a566b3d239b176b5950a959c4
Manea Palluat de Besset <manea.palluat@gmail.com>
2025-05-14 16:13:16 +0200

    feat(app): stable version with essential functionalities

--- src/app.py
+++ src/app.py
@@ -2,6 +2,9 @@ import streamlit as st
 from snowflake.snowpark import Session
 from config import SNOWFLAKE_CONFIG, validate_config
 from datetime import datetime, timezone
+import time
+import os
+
 
 #vérifier la config
 validate_config(SNOWFLAKE_CONFIG)
@@ -40,6 +43,60 @@ st.write(min_ts, max_ts) #test affichage
 # charger les valeurs distinctes pour les filtres
 namespaces = [r[0] for r in t.select("NAMESPACE").distinct().collect()]
 
+groups = [r[0] for r in t.select("GROUP_ID").distinct().collect()]
+msg_types = [r[0] for r in t.select("MESSAGE_TYPE").distinct().collect()]
+edge_nodes = [r[0] for r in t.select("EDGE_NODE_ID").distinct().collect()]
+devices = [r[0] for r in t.select("DEVICE_ID").distinct().collect()]
+
 st.sidebar.header("Filtres")
 
 sel_ns = st.sidebar.multiselect("Namespace", namespaces, default=namespaces)
+sel_grp = st.sidebar.multiselect("Group ID", groups, default=groups)
+sel_msgtp = st.sidebar.multiselect("Message Type", msg_types, default=msg_types)
+sel_edge = st.sidebar.multiselect("Edge Node ID", edge_nodes, default=edge_nodes)
+sel_dev = st.sidebar.multiselect("Device ID", devices, default=devices)
+
+print("Warehouse utilisé :", os.getenv("SNOWFLAKE_WAREHOUSE"))
+
+start_dt, end_dt = st.sidebar.slider(
+    "Date d'insertion (UTC)",
+    value=(min_ts, max_ts),
+    format="DD/MM/YYYY HH:mm",
+)
+
+start_ms = int(start_dt.timestamp() * 1000)
+end_ms = int(end_dt.timestamp() * 1000)
+
+from snowflake.snowpark.functions import col
+
+#construction du dataframe filtré
+#càd on va filtrer les données en fonction des valeurs sélectionnées dans la sidebar
+#df_raw est le dataframe brut, c'est-à-dire celui qui contient toutes les données
+df_raw = (
+    t
+    .filter(col("NAMESPACE").isin(sel_ns))
+    #filter permet de filtrer les lignes qui ne sont pas dans la liste sel_ns
+    #col permet de sélectionner une colonne
+    #isin permet de vérifier si la valeur de la colonne est dans la liste car isin renvoie un booléen
+    #
+    .filter(col("GROUP_ID").isin(sel_grp))
+    .filter(col("MESSAGE_TYPE").isin(sel_msgtp))
+    .filter(col("EDGE_NODE_ID").isin(sel_edge))
+    .filter(col("DEVICE_ID").isin(sel_dev))
+    .filter(col("INSERTED_AT").between(start_ms, end_ms))
+
+)
+
+st.write("Nombre de lignes :", df_raw.count()) #affiche le nombre de lignes
+
+total_messages = df_raw.count() #compte le nombre de lignes
+disinct_topics = df_raw.select("MSG_TOPIC").distinct().count() #compte le nombre de topics distincts
+
+k1, k2 = st.columns(2) #crée deux colonnes
+k1.metric("Nombre de messages", total_messages) #affiche le nombre de messages
+k2.metric("Nombre de topics distincts", disinct_topics) #affiche le nombre de topics distincts
+
+
+
+st.subheader("Aperçu brut (100 premières lignes)")
+st.dataframe(df_raw.limit(100).to_pandas(), use_container_width=True)
